a2cd7d8e992b50af18d17dbf543608db3f86bea8
Zandor Villarreal <z_v_h@live.com>
2024-09-20 23:05:55 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.helloandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloandroid.ui.theme.HelloAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello, $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloAndroidTheme {
+        Greeting("Android")
+    }
+}
